f6ce93b5d6a13101baba128c6a8747cfc9100add
togetic0176 <139973770+togetic0176@users.noreply.github.com>
2023-09-30 01:09:41 +0900

    typo_final-check_0930

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,30 @@
+const reference = [
+  {
+    author: "",
+    title: "",
+    year: "",
+    sourceTitle: "",
+    link: "",
+  },
+  {
+    author: "",
+    title: "",
+    year: "",
+    sourceTitle: "",
+    link: "",
+  },
+  {
+    author: "",
+    title: "",
+    year: "2",
+    sourceTitle: "",
+    link: "",
+  },
+  {
+    author: "",
+    title: "",
+    year: "",
+    sourceTitle: "",
+    link: "",
+  },
+];
